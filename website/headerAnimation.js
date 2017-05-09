80bab7dcc63fc7c0a8d819fb184dc7494f177ad3
spaghettomonsta <elenadurfee@gmail.com>
2017-05-09 12:15:07 -0600

    ldkfj

--- website/headerAnimation.js
+++ website/headerAnimation.js
@@ -24,3 +24,33 @@ $(window).scroll(function(){
         }
     }
 });
+
+
+
+
+$(function(){
+    $('#contentid').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('#contentid').data('size') == 'big')
+        {
+            $('#contentid').data('size','small');
+            $('#contentid').stop().animate({
+                top:'10vh'
+            },600);
+        }
+    }
+    else
+    {
+        if($('#contentid').data('size') == 'small')
+        {
+            $('#contentid').data('size','big');
+            $('#contentid').stop().animate({
+                top:'20vh'
+            },600);
+        }
+    }
+});
